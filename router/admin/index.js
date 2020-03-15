782794de8dc517b9482409422b0ff420c2c3f638
dapaopaopao <381281999@qq.com>
2020-03-15 14:57:55 +0800

    '3-15'

--- router/admin/index.js
+++ router/admin/index.js
@@ -1,35 +1,44 @@
 module.exports = app => {
   const express = require('express')
-  const Category = require('../../models/Category')
-  const router = express.Router()
+
+  const router = express.Router(
+    {
+      // 把父路由的路径合并到子路由路径，就是子路由可以使用父路由路径中req.params的一些参数
+      mergeParams: true
+    }
+  )
 
 
 
 
-  router.post('/categories/create', async (req, res) => {
-    const model = await Category.create(req.body)
+  router.post('/create', async (req, res) => {
+    const model = await req.Model.create(req.body)
     res.send(model)
   })
 
 
-  router.get('/categories/list', async (req, res) => {
+  router.get('/list', async (req, res) => {
     // populate是把parent（本来是字符串）转换成整个关联的上级对象 这样就可以调用上级的一些属性
-    const items = await Category.find().populate('parent').limit(10)
+    const queryOptions = {}
+    if (req.Model.modelName == 'Category') {
+      queryOptions.populate = 'parent'
+    }
+    const items = await req.Model.find().setOptions(queryOptions).limit(10)
     res.send(items)
   })
 
 
 
 
-  router.put('/categories/edit/:id', async (req, res) => {
-    const model = await Category.findByIdAndUpdate(req.params.id, req.body)
+  router.put('/edit/:id', async (req, res) => {
+    const model = await req.Model.findByIdAndUpdate(req.params.id, req.body)
     res.send(model)
   })
 
 
-  router.delete('/categories/delete/:id', async (req, res) => {
+  router.delete('/delete/:id', async (req, res) => {
     // :后面的数据都存在req.params中
-    await Category.findByIdAndRemove(req.params.id, req.body)
+    await req.Model.findByIdAndRemove(req.params.id, req.body)
     //res.send的东西存在res.data中  客户端通过res.data来获取
     res.send({
       success: 200
@@ -37,12 +46,19 @@ module.exports = app => {
   })
 
 
-  router.get('/categories/edit/:id', async (req, res) => {
+  router.get('/edit/:id', async (req, res) => {
     // :后面的数据都存在req.params中
-    const model = await Category.findById(req.params.id)
+    const model = await req.Model.findById(req.params.id)
     //res.send的东西存在res.data中  客户端通过res.data来获取
     res.send(model)
   })
 
-  app.use('/admin/api', router)
+  // 通用父路由
+  app.use('/admin/api/rest/:resource', (req, res, next) => {
+    //转换成大写单数形式 对应模型的名字
+    const modelName = require('inflection').classify(req.params.resource)
+    //传给子路由
+    req.Model = require(`../../models/${modelName}`)
+    next()
+  }, router)
 }
